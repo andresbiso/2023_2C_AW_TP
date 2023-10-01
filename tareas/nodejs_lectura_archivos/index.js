383242de3e219e987e2ee7cba0c0604c69e2abf9
Andrés Biso <biso.andres@gmail.com>
2023-10-01 20:18:52 -0300

    feat: add nodejs read file assingment

--- tareas/nodejs_lectura_archivos/index.js
+++ tareas/nodejs_lectura_archivos/index.js
@@ -1,8 +1,27 @@
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
+
 var app = express();
-app.get("/", function (request, response) {
-  response.send("Hello World!");
+app.get('/', function (req, res) {
+  res.status(200).send('¡Hola Mundo!');
+});
+app.get('/lorem-ipsum', function (req, res) {
+  const filename = './test.txt';
+  fs.readFile(filename, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('Error: ' + filename);
+      res.status(500).send(err);
+    }
+    console.log('OK: ' + filename);
+    console.log(data);
+    res.contentType('text/plain').send(data);
+  });
 });
 app.listen(10000, function () {
-  console.log("Started application on port %d", 10000);
+  const port = 10000;
+  console.log(
+    'Started application on port %d: http://localhost:%d',
+    port,
+    port,
+  );
 });
